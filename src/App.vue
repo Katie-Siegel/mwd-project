3a5e73fad53bcabe3cd8382316a2eb3e72e608aa
Katie Siegel <165830396+Katie-Siegel@users.noreply.github.com>
2026-03-30 14:41:11 -0400

    Built product info into app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <script setup>
-import CButton from './components/CButton.vue';
+import { ref } from 'vue'
+import CButton from './components/CButton.vue'
 
 
+const count = ref(0)
 </script>
 
 <template>
   <div>
-    <h1>Hello World!!!</h1></div>
-    <CButton>Click me!</CButton>  
+    <h1>New Products</h1>
+    <CButton>Click Me</CButton> 
+    <div class="product-image" @click="drawer.hidden=false">`this.product.image`</div> 
+    <div class="prduct-name">`this.product.name`</div>
+  </div>
 </template>
