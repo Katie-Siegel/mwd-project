ce81733f9f0f35a50280f0a911981afed26258df
Katie Siegel <165830396+Katie-Siegel@users.noreply.github.com>
2026-03-30 14:31:20 -0400

    Connected vue code

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <script setup>
+import CButton from './components/CButton.vue';
+
 
 </script>
 
 <template>
-  <div>Hello World!!!</div>
+  <div>
+    <h1>Hello World!!!</h1></div>
+    <CButton>Click me!</CButton>  
 </template>
