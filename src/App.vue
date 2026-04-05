c2ab91f715febcb29236cb8531cc6900a702baa7
Katie Siegel <165830396+Katie-Siegel@users.noreply.github.com>
2026-04-05 16:59:51 -0400

    Began building Vue file

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,58 @@
 <script setup>
 import { ref } from 'vue'
 import CButton from './components/CButton.vue'
-
-
-const count = ref(0)
 </script>
 
+<script src="https://cdn.jsdelivr.net/npm/vue@3/dist/vue.global.js"></script>                
+<script>
+    const options = {
+        data () {
+            return {
+                products: [],
+                selected_product_id: null,
+
+            }
+        },
+        computed: {
+            selected_product() {
+                return this.products.find ( product => {
+                    return product.id == this.selected_product_id;
+                })
+            }
+        },  
+        methods: {
+            setSelectedProduct(id) {
+                this.selected_product_id = id;
+            }
+        },
+        mounted() {
+            fetch('https://api.escuelajs.co/api/v1/products?offset=0&limit=4')
+                .then(res=>res.json())
+                .then(json=> this.products = json);
+        },
+    };
+    Vue.createApp(options).mount('#vue-app');
+</script>
+
+<div class="product-search">
+  <template v-if="products.length > 0" v-for="(product, index) in products">
+      <div class="product-card" 
+      v-if="index < 4" @click="setSelectedProduct(product.id)">
+          <img class="product-image" :src="product.images[0]">
+          <div class="product-title" v-html="product.title"></div>
+      </div>
+  </template>
+  <div v-else>Loading...</div>
+  </div>
+
+  <div v-if="selected_product" class="drawer" aria-hidden="false">
+      <img :src="selected_product.images[0]">
+      <div class="product-info">
+          <h1 class="text-5xl font-bold mb-2" v-html="selected_product.title"></h1>
+          <div class="price" v-html="'$'+selected_product.price"></div>
+          <div v-html="selected_product.description"></div>
+      </div>
+  </div>
 
 
 <template>
