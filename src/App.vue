09eed28e9cf2127a1a7bdb0059cb258e09b1aae8
Katie Siegel <165830396+Katie-Siegel@users.noreply.github.com>
2026-04-05 17:06:22 -0400

    vue script functioning

--- src/App.vue
+++ src/App.vue
@@ -1,65 +1,51 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, computed, onMounted } from 'vue'
 import CButton from './components/CButton.vue'
+
+const products = ref([])
+const selected_product_id = ref(null)
+
+const selected_product = computed(() => {
+  return products.value.find(product => product.id === selected_product_id.value)
+})
+
+function setSelectedProduct(id) {
+  selected_product_id.value = id
+}
+
+onMounted(async () => {
+  const res = await fetch('https://api.escuelajs.co/api/v1/products?offset=0&limit=4')
+  products.value = await res.json()
+})
 </script>
-
-<script src="https://cdn.jsdelivr.net/npm/vue@3/dist/vue.global.js"></script>                
-<script>
-    const options = {
-        data () {
-            return {
-                products: [],
-                selected_product_id: null,
-
-            }
-        },
-        computed: {
-            selected_product() {
-                return this.products.find ( product => {
-                    return product.id == this.selected_product_id;
-                })
-            }
-        },  
-        methods: {
-            setSelectedProduct(id) {
-                this.selected_product_id = id;
-            }
-        },
-        mounted() {
-            fetch('https://api.escuelajs.co/api/v1/products?offset=0&limit=4')
-                .then(res=>res.json())
-                .then(json=> this.products = json);
-        },
-    };
-    Vue.createApp(options).mount('#vue-app');
-</script>
-
-<div class="product-search">
-  <template v-if="products.length > 0" v-for="(product, index) in products">
-      <div class="product-card" 
-      v-if="index < 4" @click="setSelectedProduct(product.id)">
-          <img class="product-image" :src="product.images[0]">
-          <div class="product-title" v-html="product.title"></div>
-      </div>
-  </template>
-  <div v-else>Loading...</div>
-  </div>
-
-  <div v-if="selected_product" class="drawer" aria-hidden="false">
-      <img :src="selected_product.images[0]">
-      <div class="product-info">
-          <h1 class="text-5xl font-bold mb-2" v-html="selected_product.title"></h1>
-          <div class="price" v-html="'$'+selected_product.price"></div>
-          <div v-html="selected_product.description"></div>
-      </div>
-  </div>
-
 
 <template>
   <div>
     <h1>New Products</h1>
-    <CButton>Click Me</CButton> 
-    <div class="product-image" id="open-drawer-button">`this.product.image`</div> 
-    <div class="prduct-name">`this.product.name`</div>
+    <CButton>Click Me</CButton>
+
+    <div class="product-search">
+      <div v-if="products.length > 0">
+        <div 
+          v-for="product in products" 
+          :key="product.id"
+          class="product-card"
+          @click="setSelectedProduct(product.id)"
+        >
+          <img class="product-image" :src="product.images[0]" />
+          <div class="product-title">{{ product.title }}</div>
+        </div>
+      </div>
+      <div v-else>Loading...</div>
+    </div>
+
+    <div v-if="selected_product" class="drawer">
+      <img :src="selected_product.images[0]" />
+      <div class="product-info">
+        <h1>{{ selected_product.title }}</h1>
+        <div class="price">${{ selected_product.price }}</div>
+        <div>{{ selected_product.description }}</div>
+      </div>
+    </div>
   </div>
 </template>
